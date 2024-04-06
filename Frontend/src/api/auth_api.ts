5e69186acaba8fe04e64ebdd7b05a69403bfd7f0
iulitaalpetri <iulitaalpetri66@gmail.com>
2024-04-07 01:14:40 +0300

    connect frontend to backend

---
+++ Frontend/src/api/auth_api.ts
@@ -0,0 +1,89 @@
+import axios from "axios";
+import { User } from "../models/user";
+import { API_URL } from "../config";
+
+const BASE_URL = process.env.BASE_URL;
+
+interface RegistrationResponse {
+    id: string;
+    username: string;
+    email: string;
+  }
+
+
+export const registerUser = async (
+  username: string,
+  email: string,
+  password: string,
+): Promise<RegistrationResponse> => {
+  try {
+    const response = await axios.post(`${BASE_URL}/register/`, {
+      username,
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error('Registration error:', error);
+    throw new Error(error.response?.data?.detail || 'An error occurred during registration');
+  }
+};
+
+
+export const loginUser = async (
+  email: string,
+  password: string,
+): Promise<User> => {
+  try {
+    const response = await axios.post(`${BASE_URL}/login/`, {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error('Login error:', error);
+    throw new Error(error.response?.data?.detail || 'An error occurred during login');
+  }
+};
+
+export const logoutUser = async (): Promise<void> => {
+  try {
+    await axios.post(`${BASE_URL}/logout/`);
+  } catch (error: any) {
+    console.error('Logout error:', error);
+    throw new Error(error.response?.data?.detail || 'An error occurred during logout');
+  }
+};
+
+
+interface UpdateUserData {
+  username?: string;
+  email?: string;
+  password?: string;
+}
+
+// Function to update user data
+export const updateUser = async (userData: UpdateUserData): Promise<AxiosResponse<any>> => {
+  try {
+    const response = await axios.patch(`${BASE_URL}/update/`, userData, {
+      withCredentials: true // This is necessary to send cookies with the request
+    });
+    return response;
+  } catch (error: any) {
+    console.error('Update error:', error);
+    throw new Error(error.response?.data?.detail || 'An error occurred during the update');
+  }
+};
+
+// Function to delete the logged user
+export const deleteUser = async (): Promise<AxiosResponse<any>> => {
+  try {
+    const response = await axios.get(`${BASE_URL}/delete/`, {
+      withCredentials: true // This is necessary to send cookies with the request
+    });
+    return response;
+  } catch (error: any) {
+    console.error('Deletion error:', error);
+    throw new Error(error.response?.data?.detail || 'An error occurred during the deletion');
+  }
+};
---
+++ Frontend/src/models/User.model.ts
@@ -0,0 +1,13 @@
+export class User {
+    id?: number;
+    email: string;
+    photos: any[];  // Assuming you will replace 'any' with the actual type later
+    persons: any[]; // Assuming you will replace 'any' with the actual type later
+  
+    constructor(email: string) {
+      this.email = email;
+      this.photos = [];
+      this.persons = [];
+    }
+  }
+  
